861e7c1c001066f59300e0bfe049d15f2db23ba3
AlfinAkash <alfinakash2001@gmail.com>
2026-04-04 18:18:52 +0530

    fix: updated the frontend docker file

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,17 +1,7 @@
-// import axios from "axios"
-
-
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
-// })
-
-// export default API
-
 import axios from "axios"
 
-
 const API = axios.create({
-  baseURL: "http://16.176.192.179:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
 })
 
 export default API
