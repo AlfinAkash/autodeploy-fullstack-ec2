1c5de76161b3dcfbd9dc9ebabd4355ea2a490d76
AlfinAkash <alfinakash2001@gmail.com>
2026-04-04 18:10:11 +0530

    fix: temp update backend url to fronend

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,8 +1,17 @@
+// import axios from "axios"
+
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+// })
+
+// export default API
+
 import axios from "axios"
 
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+  baseURL: "http://16.176.192.179:5000",
 })
 
 export default API
