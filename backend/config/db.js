bf666a33aba9f4950034b9f14b4fa9ae32fb32b1
AlfinAkash <alfinakash2001@gmail.com>
2026-04-04 12:25:10 +0530

    fix: updated the backend sql initialization

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 import pkg from 'pg'
 import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 
 dotenv.config()
 const { Pool } = pkg
 
+// Create DB pool
 export const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -11,3 +14,18 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 })
+
+
+const initDB = async () => {
+  try {
+    const sqlPath = path.join(process.cwd(), 'backend', 'db', 'init.sql')
+    const sql = fs.readFileSync(sqlPath, 'utf8')
+
+    await pool.query(sql)
+    console.log('✅ Database tables created successfully!')
+  } catch (err) {
+    console.error('❌ Database initialization error:', err)
+  }
+}
+
+initDB()
